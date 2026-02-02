14aebd449d5d387c59c5dccf13ef95c528e7d52d
jkevin <jkevin@synopsys.com>
2026-02-02 09:31:56 +0900

    Show event info

---
+++ src/main/java/com/synopsys/pac/common/utils/ObjectUtils.java
@@ -0,0 +1,81 @@
+package com.synopsys.pac.common.utils;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+@Slf4j
+public class ObjectUtils {
+
+    public static final ObjectMapper objectMapper = objectMapper();
+
+    private static ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"))
+                .registerModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return mapper;
+    }
+
+    private ObjectUtils() {
+    }
+
+    public static String toJson(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public static String toPrettyJson(Object o) {
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public static <T> T toObject(byte[] src, Class<T> valueType) {
+        try {
+            return objectMapper.readValue(src, valueType);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public static <T> T toObject(String json, Class<T> valueType) {
+        try {
+            return objectMapper.readValue(json, valueType);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public static <T> T toObject(String json, TypeReference<T> valueTypeRef) {
+        try {
+            return objectMapper.readValue(json, valueTypeRef);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+}
